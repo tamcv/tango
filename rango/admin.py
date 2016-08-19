8cb13ab4018a9c02423e7064e84b4b600322efd2
tamcv <chauviettam@gmail.com>
2016-08-19 10:36:19 +0000

    7. Models and templates

--- rango/admin.py
+++ rango/admin.py
@@ -5,5 +5,10 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+# Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+# Update the registeration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,15 @@
+import sys
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category, Page
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,
+                    'pages': page_list}
 
     return render(request, 'rango/index.html', context_dict)
 
@@ -12,3 +17,19 @@ def about(request):
     context_dict = {'rangosay': "Tam is very handsome"}
 
     return render(request, 'rango/about.html', context_dict)
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
